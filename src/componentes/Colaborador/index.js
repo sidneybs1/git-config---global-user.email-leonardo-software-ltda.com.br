18429ae8b1087b2014e67f365cf8cca7ccc4e87a
Sidnei Batista De Souza <sidnei.souza@defensoria.ro.def.br>
2024-06-20 11:30:09 -0400

    commitando_alterações_organo

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,17 +1,15 @@
-import './Colaborador.css'
+import './colaborador.css'
 
-const Colaborador = ({nome, cargo, imagem, corDeFundo}) => {
-    return ( <div className='colaborador'>
-        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
-            <img src={imagem} alt={nome}/>
+const Colaborador = ({ colaborador, corDeFundo }) => {
+    return (<div className="colaborador">
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
-        <div className='rodape'>
-            <h4>{nome}</h4>
-            <h5>{cargo} </h5>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
         </div>
-    </div>
-
-    )
+    </div>)
 }
 
-export default Colaborador;
+export default Colaborador
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,65 +1,57 @@
-import { useState } from 'react';
-import Botao from '../Botao';
-import CampoTexto from '../CampoTexto';
-import ListaSuspensa from '../ListaSuspensa';
-import './Formulario.css';
-const Formulario = (props) => {
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './formulario.css'
+
+const Formulario = ({aoCadastrar, times}) => {
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
 
-    const aoSalvar = (evento) => {
+    const aoSubmeter = (evento) => {
         evento.preventDefault()
-        props.aoColaboradorCadastrado({
+        console.log('form enviado', nome, cargo, imagem, time )
+        aoCadastrar({
             nome,
             cargo,
             imagem,
             time
         })
-        setNome('')
-        setCargo('')
-        setImagem('')
-        setTime('')
     }
+
     return (
-        <section className='formulario'>
-            <form onSubmit={aoSalvar}>
+        <section className="formulario-container">
+            <form className="formulario" onSubmit={aoSubmeter}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Nome" 
-                    placeholder="Digite seu nome" 
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite seu nome '
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Cargo" 
-                    placeholder="Digite o seu cargo" 
+                    aoAlterado={valor => setNome(valor)}/>
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Cargo' 
+                    placeholder='Digite seu cargo '
                     valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                />
+                    aoAlterado={valor => setCargo(valor)}/>
                 <CampoTexto 
-                    label="Imagem" 
-                    placeholder="Digite o endereço da imagem"
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)} 
-                />
+                    label='Imagem' 
+                    placeholder='Informe o endereço da imagem '
+                    aoAlterado={valor => setImagem(valor)}/>
                 <ListaSuspensa 
-                    obrigatorio={true} 
-                    label="Times" 
-                    itens={props.times} 
+                    obrigatorio={true}
+                    label='Times'
+                    items={times} 
                     valor={time}
-                    aoAlterado={valor => setTime(valor)}
-                />
-                <Botao>
-                    Criar Card
-                </Botao>
+                    aoAlterado={valor => setTime(valor)}/>
+                <Botao texto='Criar card' />
             </form>
         </section>
     )
 }
 
-export default Formulario;
+export default Formulario
